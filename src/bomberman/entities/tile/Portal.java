9022a0d3c11256f8e4319418d3708223bf184ebe
TranBaLinh <43286143+TrinhBaLan@users.noreply.github.com>
2018-11-11 21:29:52 +0700

    Add files via upload

--- src/bomberman/entities/tile/Portal.java
+++ src/bomberman/entities/tile/Portal.java
@@ -7,21 +7,22 @@ import bomberman.graphics.Sprite;
 
 public class Portal extends Tile {
 	protected Board _board;
-	public Portal(int x, int y, Sprite sprite) {
+	public Portal(int x, int y, Sprite sprite, Board _board) {
 		super(x, y, sprite);
+		this._board = _board;
 	}
 	
 	@Override
 	public boolean collide(Entity e) {
 		// TODO: xử lý khi Bomber đi vào
 		if( e instanceof Bomber) {
-			if( _board.detectNoEnemies()) {
+			if( _board.detectNoEnemies() ) {
 				_board.nextLevel();
 				return true;
 			}
 			else return true;
 		}
-		return true;
+		return false;
 	}
 
 }
