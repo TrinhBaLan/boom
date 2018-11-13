b71154b7a770bfa62643fa780e6b8549f64070b5
TranBaLinh <43286143+TrinhBaLan@users.noreply.github.com>
2018-11-13 19:39:50 +0700

    Add files via upload

--- src/bomberman/entities/character/enemy/ai/AILow.java
+++ src/bomberman/entities/character/enemy/ai/AILow.java
@@ -7,8 +7,7 @@ public class AILow extends AI {
 	@Override
 	public int calculateDirection() {
 		// TODO: cài đặt thuật toán tìm đường đi
-		Random r = new Random();		
-		return r.nextInt() %4;
+		return random.nextInt(4);
 	}
 
 }
--- src/bomberman/entities/character/enemy/ai/AIMedium.java
+++ src/bomberman/entities/character/enemy/ai/AIMedium.java
@@ -1,5 +1,7 @@
 package bomberman.entities.character.enemy.ai;
 
+import java.util.Random;
+
 import bomberman.entities.character.Bomber;
 import bomberman.entities.character.enemy.Enemy;
 
@@ -15,7 +17,27 @@ public class AIMedium extends AI {
 	@Override
 	public int calculateDirection() {
 		// TODO: cài đặt thuật toán tìm đường đi
-		return 1;
+		
+		double x_bomber = _bomber.getX();
+		double y_bomber = _bomber.getY();
+		double x_enemy  = _e.getX();
+		double y_enemy  = _e.getY();
+				
+		double distance = Math.sqrt(Math.pow((x_bomber - x_enemy),2) + Math.pow((y_bomber - y_enemy),2));
+		if( distance >= 10 * 16 * Math.sqrt(2)){ // đường chéo ô vuông 10 x 10, 1 ô rộng 16 pixels
+			// giống aiLow
+			return new AILow().calculateDirection();
+		}
+		else {
+			int vertical = random.nextInt(2);
+			if( vertical == 1) { // chiều ngang
+				if( x_bomber < x_enemy) return 3;
+				else return 1;
+			}
+			else {// theo chiều dọc
+				if( y_bomber < y_enemy) return 0;
+				else return 2;
+			}
+		}
 	}
-
 }
