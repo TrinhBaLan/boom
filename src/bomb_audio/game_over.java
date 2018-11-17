d794bde2e00c02f095dd905110e79360d96fc69f
TranBaLinh <43286143+TrinhBaLan@users.noreply.github.com>
2018-11-17 19:22:13 +0700

    Add files via upload

--- src/bomb_audio/game_over.java
+++ src/bomb_audio/game_over.java
@@ -2,12 +2,11 @@ package bomb_audio;
 
 import java.io.File;
 
-public class stage_theme extends Audio{
-	File file = new File( Audio.class.getResource("/bomb_audio/audio_file/STAGE_THEME.wav").getFile() );
-	public stage_theme() {
+public class game_over extends Audio {
+	File file = new File( Audio.class.getResource("/bomb_audio/audio_file/GAME_OVER.wav").getFile() );
+	public game_over() {
 		try {
 			super.loading_file(file);
-			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -15,13 +14,10 @@ public class stage_theme extends Audio{
 	}
 	@Override
 	public void play(boolean loop) {
+		clip.setFramePosition(0);
 		if( loop) {		
 			clip.loop(clip.LOOP_CONTINUOUSLY);
 		}
 		else clip.loop(0);
-	}
-	
-	public void stop() {
-		clip.stop();
 	}
 }
